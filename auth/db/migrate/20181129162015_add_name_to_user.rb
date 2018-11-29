7d37aa233d85fbbfa431dedb2abde8ebd5bceb15
Guillermo Mora <guillermo@cleverppc.com>
2018-11-29 17:36:06 +0100

    Finish register and login

---
+++ auth/db/migrate/20181129162015_add_name_to_user.rb
@@ -0,0 +1,8 @@
+class AddNameToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :surname, :string
+    add_column :users, :role, :string
+    add_column :users, :grade, :string
+  end
+end
--- auth/db/schema.rb
+++ auth/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_151713) do
+ActiveRecord::Schema.define(version: 2018_11_29_162015) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2018_11_29_151713) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "surname"
+    t.string "role"
+    t.string "grade"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
