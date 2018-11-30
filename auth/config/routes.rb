f2fb919e353763db8e0f4f4d1b5d09563da72f80
Guillermo Mora <guillermo@cleverppc.com>
2018-11-30 17:08:20 +0100

    Fix merge

--- auth/config/routes.rb
+++ auth/config/routes.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
-
   resources :machines
-  #devise_for :users
->>>>>>> ddbbc9ec386e24ddddc3be4f00b94761a7a0ef6b
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
   devise_scope :user do
@@ -27,10 +22,6 @@ Rails.application.routes.draw do
   get 'verify_test/:user_domain_token', to: 'stuks#verify_test', as: 'verify_test', defaults: { format: :json }
   get 'verify/:user_domain_token', to: 'stuks#verify', as: 'verify', defaults: { format: :json }
   get 'generate_keys', to: 'stuks#generate_keys', as: 'generate_keys'
-<<<<<<< HEAD
   get 'admin_dashboard', to: 'stuks#admin_dashboard', as: 'admin_dashboard'
-end
-=======
   get 'generate_config/:id', to: 'stuks#generate_config', as: 'generate_config'
 end
->>>>>>> ddbbc9ec386e24ddddc3be4f00b94761a7a0ef6b
