3f679039a696f33665c3360c3a453b254465faa0
vrandkode <vrandkode@gmail.com>
2018-11-30 15:07:43 +0100

    stuk-supervisor. create the skeleton to supervise the final systems

--- supervisor/agent.py
+++ supervisor/agent.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python
+agent.py#!/usr/bin/python
 from concurrent.futures import ThreadPoolExecutor
 from crypto             import OpenPrivateKey
 from subprocess         import call
+from provision          import handleRemoteRealm
 import scapy.all as scapy
 import requests
+import constant
 
 #Platform constants
 globals.platformKey = None
@@ -12,15 +14,26 @@ globals.platformKey = None
 executor = ThreadPoolExecutor(max_workers=10)
 
 #Async. who process the packet by worker
-def process(publickey, user, domain, totp, err):
-    print(publickey, user, domain, totp, err)
+def process(publickey, user, destinations, totp, err):
+    print(publickey, user, destinations, totp, err)
     r = requests.get(constant.AUTH_MOCK_ENDPOINT)
+
+    if r.text.result is "200":
+        handleRemoteRealm(user, publickey, destinations)
+
     print(r.text)
 
 # Simple approach to add user into authorisation context
 def registerPublicKey(pubkey):
     with open(constant.AUTHORIZED_KEYS_PATH, "a") as auth:
         auth.write(pubkey)
+
+# Extraction of authentication token from tcp packet
+def extractAuthenticationToken(payload):
+    """ @todo: extract/parse from packet the encrypted block stream """
+    if not payload:
+        return None
+    return b"AAAAAAA"
 
 # Packet parser needed for the sniffer
 def parser(packet):
@@ -30,10 +43,11 @@ def parser(packet):
         if not token:
             return
 
-        publickey, user, domain, totp, err = AD(globals.platformKey, token)
+        #Decrypt the token and obtains the tuple of honor
+        publickey, user, destinations, totp, err = AD(globals.platformKey, token)
         if (err is not None):
             return
-        executor.submit(process())
+        executor.submit(process(publickey, user, destinations, totp, err))
 
     except er:
         print("Error", er)
@@ -41,9 +55,9 @@ def parser(packet):
 def hook():
     """ Parsing the traffic packets """
     print("[] Listening to device...")
-    scapy.sniff(filter='ip proto \\tcp and ((tcp dst port 4000 or 4001 or 4002 or 22) and tcp[tcpflags] & tcp-syn != 0)',prn=parser)
+    scapy.sniff(filter=constant.DEFAULT_FILTER_SEQUENCE, prn=parser)
 
 if __name__ == "__main__":
-    globals.platformKey = OpenPrivateKey(".keys/platform.key")
+    globals.platformKey = OpenPrivateKey(constant.PLATFORM_KEYS_PATH)
     hook()
    
