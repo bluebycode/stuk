ab80980c5209a990a07b9711c5ef716d3ae90b0d
vrandkode <vrandkode@gmail.com>
2018-12-01 13:07:04 +0100

    stuk-supervisor.changes

--- supervisor/constant.py
+++ supervisor/constant.py
@@ -1,12 +1,19 @@
-# Mock constants
+ # Mock constants
 
 #Endpoints
 AUTH_MOCK_ENDPOINT   = 'http://172.16.64.77:3000/verify/YWx2YXJvL3plbC5lZHUvNDg1OTQ1'
 
 #Paths
-AUTHORIZED_KEYS_PATH = "/home/cybercamp/.ssh/.authorizedkeys"
+AUTHORIZED_KEYS_PATH = "/home/cybercamp/.ssh/authorized_keys"
+
 PLATFORM_KEYS_PATH   = ".keys/platform.key"
 
 #Communication
 DEFAULT_FILTER_SEQUENCE = 'ip proto \\tcp and ((tcp dst port 4000 or 4001 or 4002 or 22) and tcp[tcpflags] & tcp-syn != 0)'
 
+
+#Authentication service
+VERIFICATION_PATH = "http://172.16.64.77:3000/verify/{0}"
+
+
+SUPERVISOR_KEY = "/home/cybercamp/supervisor/supervisor/agent/.keys/platform.pem"
--- supervisor/crypto.py
+++ supervisor/crypto.py
@@ -4,36 +4,43 @@ import base64
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
-def OpenPrivateKey(path):
-    """ Open a private key from a file system location """
-    return RSA.importKey(open(path).read())
+def ParsePublicKey(path):
+    return RSA.importKey(open(path).read()).publickey().exportKey('OpenSSH')
+
+def Keys():
+    key = RSA.generate(2048, e=65537)
+    return key, key.publickey()
+
+def PairKey(plain=False, size=2048):
+    """ Genera par de claves RSA """
+    key = RSA.generate(size)
+    return (key, key.publickey()) if plain else (key.exportKey("PEM"), key.publickey().exportKey('OpenSSH'))
 
 def AD(rsa, cipheredmessage):
     """ RSA - Asymetric Decryptation mediante PKCS1_OAEP """
-    key = RSA.importKey(open(rsa).read())
+    key = RSA.importKey(open(rsa, "rb").read())
     cipher = PKCS1_OAEP.new(key)
     block = 256
     offset = 0
     encrypted = base64.b64decode(cipheredmessage)
-    decrypted=b""
+    decrypted = b""
     while offset < len(encrypted):
         chunk = encrypted[offset:offset + block]
-        print(len(chunk))
-        decrypted+= cipher.decrypt(chunk)
+        decrypted+=cipher.decrypt(chunk)
         offset+=block
-    return decrypted
+    return zlib.decompress(decrypted)
 
 def AE(rsa, message):
     """ RSA - Asymetric Encriptation mediante PKCS1_OAEP """
-    key = RSA.importKey(open(rsa).read())
+    key = RSA.importKey(open(rsa, "rb").read())
     cipher = PKCS1_OAEP.new(key)
-    #message = zlib.compress(message)
-    block=128
-    offset=0
-    blocks=True
+    compressed = zlib.compress(message)
+    block  = 214
+    offset = 0
+    blocks = True
     encrypted=b""
     while blocks:
-        chunk = message[offset:offset + block]
+        chunk = compressed[offset:offset + block]
         if len(chunk) % block !=0:
             blocks=False
             chunk +=b'.' * (block - len(chunk))
